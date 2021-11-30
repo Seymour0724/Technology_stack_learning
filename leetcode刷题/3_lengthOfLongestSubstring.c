08ac8febaf5e1dc9ddffbd6ec85b338399f3462f
gtz_govalley <603415887@qq.com>
2021-11-30 16:01:58 +0800

    leetcode_c_3

---
+++ leetcode刷题/3_lengthOfLongestSubstring.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<malloc.h>
+
+int lengthOfLongestSubstring(char * s){
+    int max=1;
+    int sSize = sizeof(s)/sizeof(s[0]);
+    for(int i=0; i<sSize; i++)
+    {   
+        int j = i+1;
+        for(j; j<sSize; j++)
+        {
+            char *ss = 
+        }
+    }
+}
+
+int main()
+{
+    return 0;
+}

d602713eb6cee60065bf6ae93f6db194a3735a1f
gtz_govalley <603415887@qq.com>
2021-12-10 16:00:39 +0800

    C语言学习第八天

--- C语言学习/1_datatype.c
+++ C语言学习/1_datatype.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 #include<float.h>
+#include<stdlib.h>
+
+typedef struct{
+	unsigned int nMant:23;
+	unsigned int nExp:8;
+	unsigned int nSign:1;
+} FP_SINGLE;
 
 void type_size()
 {
@@ -9,8 +16,29 @@ void type_size()
 	printf("float max value: %e\n", FLT_MAX);
 }
 
+void test1(){
+	int a = 0x10000000;
+	int b = 0xffffffff;
+	printf("a = %d\nb = %d\n",a,b);
+}
+
+void float_strucr(){
+	char strBin[33] = {0};
+	float f = -0.019625;
+	FP_SINGLE *p = (FP_SINGLE*)&f;
+
+	itoa(p->nSign,strBin,2);
+	printf("sign: %s\n", strBin);
+	itoa(p->nExp,strBin,2);
+	printf("exp: %s\n", strBin);
+	itoa(p->nMant,strBin,2);
+	printf("mant: %s\n", strBin);
+}
+
 int main()
 {
-	type_size();
+	// type_size();
+	// test1();
+	float_strucr();
 	return 0;
 }
--- leetcode刷题/29_divide.c
+++ leetcode刷题/29_divide.c
@@ -1,24 +1,75 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<stdbool.h>
 
 int divide(int dividend, int divisor){
     int low = pow(-2,31);
     int up = pow(2,31)-1;
+    int flag = 0;
     if((dividend<low||dividend>up)||(divisor<low||divisor>up)){
         return up;
     }
+    if((dividend>0&&divisor>0)||(dividend<0&&divisor<0)){
+        flag = 1;
+    }
+    dividend = abs(dividend);
+    divisor = abs(divisor);
     if(dividend<divisor) return 0;
-    int res=1;
-    while(divisor<dividend){
-        res++;
-        divisor = divisor<<1;
+    int res=1,temp=divisor;
+    while(temp<dividend){
+        temp+=divisor;
+        if(temp<=dividend){
+            res++;
+        }
         //printf("%d\n",divisor);
     }
-    return divisor==dividend? res+1:res;
+    // int result = divisor==dividend? res+1:res;
+    return flag ? res : 0-res;
 }
 
+int divide2(int dividend, int divisor){
+    if(dividend == 0)
+        return 0;
+    if(dividend == INT_MIN && divisor == -1)
+        return INT_MAX;
+    if(dividend == INT_MIN && divisor == 1)
+        return INT_MIN;
+    if(dividend == INT_MIN && divisor == INT_MAX)
+        return -1;
+    if(dividend == divisor)
+        return 1;
+    if(divisor == INT_MIN)
+        return 0;
+    int sum1 = 0,t1 = 0;
+    int sum2 = 0,t2 = 0;
+    int sum3 = 0,t3 = 0;
+    int res = 0;
+    bool flag = (dividend ^ divisor) < 0;
+    int result = 0;
+    if(dividend == INT_MIN){
+        dividend += abs(divisor);
+        result++;
+    }
+    int t = abs(dividend);
+    int d = abs(divisor);
+    for(int i = 31;i >= 0;i--){
+        if((t >> i) >= d){
+            result += 1 << i;
+            t -= d << i;
+        }
+    }
+    if(result == INT_MIN)
+        return INT_MAX;
+    else
+        return flag ? -result : result;
+    return 0;
+}
+
+
 int main(){
-    int a=12, b=3;
-    printf("%d\n",divide(a,b));
+    int a=121, b=61;
+    printf("%d\n",divide2(a,b));
     return 0; 
 }
